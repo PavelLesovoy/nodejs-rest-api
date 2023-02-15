47b8be0107cf558c156f0d49ec043f5d45b27171
PavelLesovoy <98907227+PavelLesovoy@users.noreply.github.com>
2023-02-15 15:55:18 +0200

    mongodb HW done

--- CRUD/contacts/postContact.js
+++ CRUD/contacts/postContact.js
@@ -1,15 +1,14 @@
-const HttpError = require("../../helpers");
-const contacts = require("../../models/contacts");
-const { addSchema } = require("../../schemas/contacts");
+const { HttpError } = require("../../helpers");
+const { Contact } = require("../../models/contacts");
+const { schemas } = require("../../models/contacts");
 
 const postContact = async (req, res, next) => {
+  const { error } = schemas.addSchema.validate(req.body);
   try {
-    const { error } = addSchema.validate(req.body);
-
     if (error) {
       throw HttpError(400, "missing required name field");
     }
-    const result = await contacts.addContact(req.body);
+    const result = await Contact.create(req.body);
 
     res.status(201).json(result);
   } catch (error) {
